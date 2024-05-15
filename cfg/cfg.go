de0618ab03db523b8cc62050f4f2808389e4d16e
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2024-05-16 06:40:18 +0700

    feat: update config load to load only once

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 	//"github.com/joho/godotenv"
 )
 
@@ -27,38 +28,44 @@ type Config struct {
 	S3_REGION      string
 }
 
+var (
+	config     Config
+	configOnce sync.Once
+)
+
 func LoadConfig() (Config, error) {
 	var err error
 
-	// Load the .env file before reading the environment variables.
-	// err = godotenv.Load(".env")
-	// if err != nil {
-	// 	fmt.Printf("No .env file found or error loading it: %v\n", err)
-	// }
+	configOnce.Do(func() {
+		// Load the .env file before reading the environment variables.
+		// err = godotenv.Load(".env")
+		// if err != nil {
+		// 	fmt.Printf("No .env file found or error loading it: %v\n", err)
+		// }
 
-	config := Config{
-		DbName:     GetEnvOrDefault("DB_NAME", "default_db_name"),
-		DbHost:     GetEnvOrDefault("DB_HOST", "localhost"),
-		DbPort:     GetEnvOrDefault("DB_PORT", "5432"),
-		DbUsername: GetEnvOrDefault("DB_USERNAME", "user"),
-		DbPassword: GetEnvOrDefault("DB_PASSWORD", "password"),
+		config = Config{
+			DbName:     GetEnvOrDefault("DB_NAME", "default_db_name"),
+			DbHost:     GetEnvOrDefault("DB_HOST", "localhost"),
+			DbPort:     GetEnvOrDefault("DB_PORT", "5432"),
+			DbUsername: GetEnvOrDefault("DB_USERNAME", "user"),
+			DbPassword: GetEnvOrDefault("DB_PASSWORD", "password"),
 
-		APPPort: GetEnvOrDefault("APP_PORT", "8080"),
-		ENV:     GetEnvOrDefault("ENV", "development"),
+			APPPort: GetEnvOrDefault("APP_PORT", "8080"),
+			ENV:     GetEnvOrDefault("ENV", "development"),
 
-		JWTSecret: GetEnvOrDefault("JWT_SECRET", "sec"),
+			JWTSecret: GetEnvOrDefault("JWT_SECRET", "sec"),
 
-		S3_ID:          GetEnvOrDefault("JWT_SECRET", ""),
-		S3_SECRET_KEY:  GetEnvOrDefault("JWT_SECRET", ""),
-		S3_BUCKET_NAME: GetEnvOrDefault("JWT_SECRET", ""),
-		S3_REGION:      GetEnvOrDefault("JWT_SECRET", ""),
-	}
+			S3_ID:          GetEnvOrDefault("S3_ID", ""),
+			S3_SECRET_KEY:  GetEnvOrDefault("S3_SECRET_KEY", ""),
+			S3_BUCKET_NAME: GetEnvOrDefault("S3_BUCKET_NAME", ""),
+			S3_REGION:      GetEnvOrDefault("S3_REGION", ""),
+		}
 
-	config.BcryptSalt, err = strconv.Atoi(GetEnvOrDefault("BCRYPT_SALT", "10"))
-	if err != nil {
-		err = fmt.Errorf("failed to convert BCRYPT_SALT to int: %v", err)
-		return config, err
-	}
+		config.BcryptSalt, err = strconv.Atoi(GetEnvOrDefault("BCRYPT_SALT", "10"))
+		if err != nil {
+			err = fmt.Errorf("failed to convert BCRYPT_SALT to int: %v", err)
+		}
+	})
 
 	return config, err
 }
